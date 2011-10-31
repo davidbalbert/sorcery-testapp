bfb7426b6db23b0bbe65b42e4f800ca709b805b1
David Albert <davidbalbert@gmail.com>
2011-10-31 17:21:44 -0400

    auto login new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(params[:user])
     if @user.save
+      auto_login(@user)
       redirect_to(root_url)
     else
       render :new
     end
 
   end
-  
-
 end
